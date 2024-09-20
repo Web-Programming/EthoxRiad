99a3552970f3d2997e45899f60100e7e471eb2a1
Fanesz <fanes23.pratama@mhs.mdp.ac.id>
2024-09-20 18:18:31 +0700

    edit: responsive sidebar

--- src/common/components/Layout/Sidebar.tsx
+++ src/common/components/Layout/Sidebar.tsx
@@ -4,11 +4,13 @@ import { GrSchedules } from "react-icons/gr";
 import { useSidebar } from "@store/sidebar";
 import { useNavigate } from "react-router-dom";
 import clsx from "clsx";
+import useWindowSize from "@hooks/useWindowSize";
 
 const Sidebar = () => {
   const currentMenu = useSidebar();
   const iconClass = "w-5 h-5 text-white ";
   const navigate = useNavigate();
+  const { md } = useWindowSize();
 
   const sidebarItems = [
     {
@@ -17,7 +19,7 @@ const Sidebar = () => {
       icon: <RxDashboard className={iconClass} />,
     },
     {
-      title: "Jadwal Perkuliahan",
+      title: "Perkuliahan",
       route: "/jadwal-perkuliahan",
       icon: <GrSchedules className={iconClass} />,
     },
@@ -29,24 +31,32 @@ const Sidebar = () => {
   };
 
   return (
-    <div className="bg-secondary rounded-s-3xl w-24 flex flex-col items-center py-12 gap-1">
+    <div className="bg-secondary rounded-s-3xl md:w-56 w-24 flex flex-col items-center py-12 gap-1">
       {sidebarItems.map((item, index) => (
-        <div key={index} className="w-full h-10 px-4">
+        <div key={index} className="w-full h-12 px-4">
           <Tooltip
             hasArrow
-            label={item.title}
+            label={!md && item.title}
             placement="end"
             className="bg-slate-800 text-white px-2 py-1 rounded-md text-sm"
           >
             <button
               className={clsx(
-                "w-full h-full flex items-center justify-center rounded-lg",
-                "hover:bg-blue-500/20",
-                currentMenu.menu === index && "bg-blue-500/50",
+                "w-full h-full flex items-center justify-left rounded-lg",
+                currentMenu.menu === index
+                  ? "bg-blue-500/40"
+                  : "hover:bg-blue-500/20",
               )}
               onClick={() => handleClick(index)}
             >
-              {item.icon}
+              <span className="flex gap-2 items-center ps-4">
+                {item.icon}
+                {md && (
+                  <span className="text-white text-sm font-semibold">
+                    {item.title}
+                  </span>
+                )}
+              </span>
             </button>
           </Tooltip>
         </div>
